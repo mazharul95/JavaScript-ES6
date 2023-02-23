752063ed58ffdbb61f51eb49569fa27c51f5941f
mazharul95 <mipiyas07@gmail.com>
2023-02-23 23:14:25 +0600

    ES6 intro var, Let and const

---
+++ js/let-const.js
@@ -0,0 +1,49 @@
+/*
+The traditional ES5 type syntax for declaring and initializing a variable is as follows −
+*/
+//Declaration using var keyword
+// var variable_name = value;
+
+var personName = "Tom";
+console.log("The value in the variable is: " + personName);
+
+//let:let it to reassign
+let money = 25;
+money = 35;
+console.log(money);
+
+/*ES6 introduces the following variable declaration syntax −
+
+Using the let.
+Using the const.
+*/
+
+//const: do not allow it to reassign;
+// const cant accept new values
+//const: allow it to previous value reassign or updating;
+const bird = "moyna pakhi";
+const message = bird + " listen human command";
+console.log(message);
+const numbers = [12, 89, 65, 45];
+numbers[1] = 99;
+
+// numbers = [99, 102, 77, 65, 55, 22];
+
+numbers.push(123);
+console.log(numbers);
+
+//object declare
+const student = {
+  name: "mofiz",
+  address: "Rongpur",
+};
+//object value change
+student.name = "mofazzol";
+console.log(student);
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  sum = sum + number;
+  console.log(number);
+}
