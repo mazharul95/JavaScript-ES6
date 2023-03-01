ca426423afa75f2f903bad24e349e23697c82bb8
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-01 14:17:16 +0600

    Load posts and display on the website with CSS

--- explore-api/js/fetch.js
+++ explore-api/js/fetch.js
@@ -16,6 +16,6 @@ fetch(url)
 function loadData() {
   const url = "https://jsonplaceholder.typicode.com/todos/1";
   fetch(url)
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then(response => response.json())
+    .then(json => console.log(json));
 }
