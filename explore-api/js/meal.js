a0ab63485afac3b0b8ed7d057af50a79294238a6
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-02 17:31:11 +0600

    Display dynamic search result using bootstrap cards

--- explore-api/js/meal.js
+++ explore-api/js/meal.js
@@ -1,5 +1,7 @@
-const loadMeals = () =>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals = (searchText) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    console.log(url);
+    fetch(url)
     .then(res =>res.json())
     .then(data => displayMeals(data.meals));
 }
@@ -7,30 +9,38 @@ const loadMeals = () =>{
 const displayMeals = meals =>{
      //console.log(meals);
      //step 1:container element
-     const mealContainer = document.getElementById('meal-container');
+     const mealsContainer = document.getElementById('meals-container');
+     mealsContainer.innerHTML= '';
      meals.forEach(meal => {
         console.log(meal);
         //step 2: create child for each element
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
-
         //step 3: set content of the child
         mealDiv.innerHTML = `
         <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
+            </div>
         </div>
         `;
         //step 4: appendChild
-        mealContainer.appendChild(mealDiv)
+        mealsContainer.appendChild(mealDiv);
+
 
      })
 }
 
+const searchMeal= () =>{
+    const searchText = document.getElementById('search-field').value;
+    //search meals
+    console.log(searchText);
+    loadMeals(searchText);
 
 
+}
 
-loadMeals();
+
+loadMeals('rice');
