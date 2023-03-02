44b0da9db52cb1ee6c58f32880df261bed895707
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-02 16:05:29 +0600

    advanced: Call dynamic API, load dynamic data to display

--- explore-api/js/countries.js
+++ explore-api/js/countries.js
@@ -16,11 +16,33 @@ const displayCountries = countries =>{
         countryDiv.innerHTML = `
          <h3>Name: ${country.name.common}</h3>
         <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
-        
+        <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
 
-    });
+    })
+}
+
+const loadCountryDetails = code => {
+    //console.log('details coming soon')
+    const url =`https://restcountries.com/v3.1/alpha/${code}`;
+    //console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetail(data[0]))
+}
+
+const showCountryDetail = country =>{
+    const detailContainer = document.getElementById('country-detail');
+    detailContainer.innerHTML= `
+    <h3>Name: ${country.name.common} </h3>
+    <img src="${country.flags.png}">
+    
+    
+    
+    `
+
+
 }
 
 
