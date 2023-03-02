f5aba8612a002d589d610ea31bb59d474373626f
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-02 13:18:29 +0600

    Handle Random user with nested API data

---
+++ explore-api/js/randomUser.js
@@ -0,0 +1,32 @@
+const loadUser = () => {
+    fetch('https://randomuser.me/api/?gender=female')
+    .then(res => res.json())
+    .then(data => displayUser(data));
+
+}
+const displayUser = user =>{
+    const genderTag = document.getElementById('gender');
+    genderTag.innerHTML = user.results[0].gender;
+    console.log(user.results[0].gender); //title
+
+    const name = user.results[0].name.title  + ' ' + user.results[0].name.first + ' ' + user.results[0].name.last;
+    document.getElementById('name').innerHTML = name;
+    console.log(user.results[0].name);
+
+    const email = user.results[0].email;
+    document.getElementById('email').innerHTML = email;
+    console.log(user.results[0].email);
+
+    const dob = user.results[0].dob.age + ' ' + 'Years' +' '+ user.results[0].dob.date;
+    document.getElementById('dob').innerHTML = dob;
+    console.log(user.results[0].dob.date);
+
+    const location = 'StreetNumber: ' + user.results[0].location.street.number +','+ ' ' + 'StreetName:'+ user.results[0].location.street.name+', ' + '' + 'City: ' + user.results[0].location.city;
+    document.getElementById('location').innerHTML = location;
+    console.log(user.results[0].location.street.name);
+
+
+
+}
+
+loadUser();
